b2f08f901e99b7766348fed0effb66cf3211e7e8
Courtney Phillips <courtney.mae.phillips@gmail.com>
2016-02-28 14:24:40 -0800

    add zipLookup method to lookup-form component. call component in index template.

--- app/components/lookup-form.js
+++ app/components/lookup-form.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    zipLookup() {
+      var params = {
+        zipcode: this.get('zip')
+      };
+      this.sendAction('zipLookup', params)
+    }
+  }
 });
